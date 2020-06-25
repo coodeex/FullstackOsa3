6cd673837d15334fdc5af367abc844ea18d6359d
coodeex <juhani.s.lehto@gmail.com>
2020-06-25 18:41:59 +0300

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.json()) 
 app.use(morgan(function (tokens, req, res) {
   const body = JSON.stringify(req.body) !== "{}" ? JSON.stringify(req.body) : null
