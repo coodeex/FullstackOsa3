57dd224fa446b32b64e58d612298187dc7ab3203
coodeex <juhani.s.lehto@gmail.com>
2020-06-26 07:26:42 +0300

    frontend build

---
+++ build/precache-manifest.ecbc888615f5632a063de8593a113ba7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82dac7dc62ede0b937123157020e0f49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25a3600c1dc9b90d5fa2",
+    "url": "/static/js/2.4baeefc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4baeefc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545dcff087423af92f7f",
+    "url": "/static/js/main.bdfa9f47.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
