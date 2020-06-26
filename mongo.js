a65eb83610240c93c348d5382cf11ac2ba105917
coodeex <juhani.s.lehto@gmail.com>
2020-06-26 16:30:24 +0300

    mongo.js works (3.12)

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 5 && process.argv.length != 3) {
+  console.log('give all arguments ( node mongo.js <password> <name> <number> )')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://juhax:${password}@cluster0-7xqio.mongodb.net/phonebook0?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+  console.log("phonebook:")
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if (process.argv.length == 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number
+  })
+
+  person.save().then(response => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
